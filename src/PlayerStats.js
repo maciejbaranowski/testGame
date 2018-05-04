221de21c4e00cc0a2431049e7b530a4267b70401
Maciej Baranowski <maciekbaranowski@onet.pl>
2018-05-04 11:27:07 +0200

    Bootstrap changed to custom theme

--- src/PlayerStats.js
+++ src/PlayerStats.js
@@ -1,13 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
-import {
-  ProgressBar,
-  Badge,
-  Alert,
-  Fade,
-  ListGroup,
-  ListGroupItem
-} from "react-bootstrap";
+import { ProgressBar, Badge, Alert, Fade, ListGroup, ListGroupItem } from "react-bootstrap";
 
 const PlayerStats = props => (
   <div>
@@ -17,25 +10,17 @@ const PlayerStats = props => (
     <ListGroup>
       <ListGroupItem key="health">
         <span>🧡</span> Health
-        <ProgressBar
-          now={props.health}
-          label={props.health + "/100"}
-          bsStyle="danger"
-        />
+        <ProgressBar now={props.health} label={props.health + "/100"} bsStyle="danger" />
       </ListGroupItem>
       <ListGroupItem key="fatigue">
         <span>😫</span> Fatigue
-        <ProgressBar
-          now={props.fatigue}
-          label={props.fatigue + "/100"}
-          bsStyle="success"
-        />
+        <ProgressBar now={props.fatigue} label={props.fatigue + "/100"} bsStyle="success" />
       </ListGroupItem>
       <ListGroupItem key="cash">
-        <span>💰</span> Cash <Badge>{props.cash}</Badge>
+        <span>💰</span> Cash <Badge bsStyle="primary">{props.cash}</Badge>
       </ListGroupItem>
       <ListGroupItem key="skills">
-        ️ <span>⚔️</span> Fighting skill <Badge>{props.skills.fight}</Badge>
+        ️ <span>⚔️</span> Fighting skill <Badge bsStyle="primary">{props.skills.fight}</Badge>
       </ListGroupItem>
     </ListGroup>
     <Fade in={props.status.length > 0}>
